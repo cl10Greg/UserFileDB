034239f09f2b360e0ecbdea627835cae7f4c673a
cl10Greg <cl10Greg>
2014-02-10 16:29:26 -0500

    Initial commit

---
+++ UserFileDB/Form1.Designer.vb
@@ -0,0 +1,181 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.loadPlainBtn = New System.Windows.Forms.Button()
+        Me.loadEncryptedBtn = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.userTxt = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.passTxt = New System.Windows.Forms.TextBox()
+        Me.newPlainBtn = New System.Windows.Forms.Button()
+        Me.userEncyptedBtn = New System.Windows.Forms.Button()
+        Me.binBtn = New System.Windows.Forms.Button()
+        Me.loadBinBtn = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(66, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "File Content:"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(15, 36)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(335, 150)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'loadPlainBtn
+        '
+        Me.loadPlainBtn.Location = New System.Drawing.Point(15, 192)
+        Me.loadPlainBtn.Name = "loadPlainBtn"
+        Me.loadPlainBtn.Size = New System.Drawing.Size(75, 23)
+        Me.loadPlainBtn.TabIndex = 2
+        Me.loadPlainBtn.Text = "Load Plain"
+        Me.loadPlainBtn.UseVisualStyleBackColor = True
+        '
+        'loadEncryptedBtn
+        '
+        Me.loadEncryptedBtn.Location = New System.Drawing.Point(96, 192)
+        Me.loadEncryptedBtn.Name = "loadEncryptedBtn"
+        Me.loadEncryptedBtn.Size = New System.Drawing.Size(96, 23)
+        Me.loadEncryptedBtn.TabIndex = 3
+        Me.loadEncryptedBtn.Text = "Load Encypted"
+        Me.loadEncryptedBtn.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(372, 36)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Username:"
+        '
+        'userTxt
+        '
+        Me.userTxt.Location = New System.Drawing.Point(436, 33)
+        Me.userTxt.Name = "userTxt"
+        Me.userTxt.Size = New System.Drawing.Size(100, 20)
+        Me.userTxt.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(372, 63)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(56, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Password:"
+        '
+        'passTxt
+        '
+        Me.passTxt.Location = New System.Drawing.Point(436, 60)
+        Me.passTxt.Name = "passTxt"
+        Me.passTxt.Size = New System.Drawing.Size(100, 20)
+        Me.passTxt.TabIndex = 7
+        Me.passTxt.UseSystemPasswordChar = True
+        '
+        'newPlainBtn
+        '
+        Me.newPlainBtn.Location = New System.Drawing.Point(375, 101)
+        Me.newPlainBtn.Name = "newPlainBtn"
+        Me.newPlainBtn.Size = New System.Drawing.Size(75, 23)
+        Me.newPlainBtn.TabIndex = 8
+        Me.newPlainBtn.Text = "Plain User"
+        Me.newPlainBtn.UseVisualStyleBackColor = True
+        '
+        'userEncyptedBtn
+        '
+        Me.userEncyptedBtn.Location = New System.Drawing.Point(461, 101)
+        Me.userEncyptedBtn.Name = "userEncyptedBtn"
+        Me.userEncyptedBtn.Size = New System.Drawing.Size(75, 23)
+        Me.userEncyptedBtn.TabIndex = 9
+        Me.userEncyptedBtn.Text = "Encypted User"
+        Me.userEncyptedBtn.UseVisualStyleBackColor = True
+        '
+        'binBtn
+        '
+        Me.binBtn.Location = New System.Drawing.Point(542, 101)
+        Me.binBtn.Name = "binBtn"
+        Me.binBtn.Size = New System.Drawing.Size(75, 23)
+        Me.binBtn.TabIndex = 10
+        Me.binBtn.Text = "Binary"
+        Me.binBtn.UseVisualStyleBackColor = True
+        '
+        'loadBinBtn
+        '
+        Me.loadBinBtn.Location = New System.Drawing.Point(198, 192)
+        Me.loadBinBtn.Name = "loadBinBtn"
+        Me.loadBinBtn.Size = New System.Drawing.Size(75, 23)
+        Me.loadBinBtn.TabIndex = 11
+        Me.loadBinBtn.Text = "Load Binary"
+        Me.loadBinBtn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(783, 225)
+        Me.Controls.Add(Me.loadBinBtn)
+        Me.Controls.Add(Me.binBtn)
+        Me.Controls.Add(Me.userEncyptedBtn)
+        Me.Controls.Add(Me.newPlainBtn)
+        Me.Controls.Add(Me.passTxt)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.userTxt)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.loadEncryptedBtn)
+        Me.Controls.Add(Me.loadPlainBtn)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents loadPlainBtn As System.Windows.Forms.Button
+    Friend WithEvents loadEncryptedBtn As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents userTxt As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents passTxt As System.Windows.Forms.TextBox
+    Friend WithEvents newPlainBtn As System.Windows.Forms.Button
+    Friend WithEvents userEncyptedBtn As System.Windows.Forms.Button
+    Friend WithEvents binBtn As System.Windows.Forms.Button
+    Friend WithEvents loadBinBtn As System.Windows.Forms.Button
+
+End Class
---
+++ UserFileDB/Form1.vb
@@ -0,0 +1,265 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private Sub newPlainBtn_Click(sender As Object, e As EventArgs) Handles newPlainBtn.Click
+        Dim plainPath As String = "C:\Work\Research\Visual Basic\UserFileDB\Resources\userPlain.ccorp"
+
+        Dim createFileAns
+        Dim userString As String
+
+        userString = userTxt.Text & "," & passTxt.Text & vbCrLf
+
+        'Plain Test
+        If File.Exists(plainPath) Then
+            If checkIfFileEmpty(plainPath) Then
+                MsgBox("File is empty.  Writing")
+                File.WriteAllText(plainPath, userString)
+                'Dim sw As New StreamWriter(plainPath)
+                'sw.Write(userString)
+                'sw.Close()
+            Else
+                MsgBox("File is not empty.  Appending.")
+                File.AppendAllText(plainPath, userString)
+                'Dim sw As New StreamWriter(plainPath)
+                'sw.WriteLine(userString)
+                'sw.Close()
+            End If
+        Else
+            createFileAns = MsgBox("File doesn't exist.  Create it?", MsgBoxStyle.YesNo)
+            If createFileAns = vbYes Then
+                MsgBox("File created")
+                File.Create(plainPath)
+            Else
+                MsgBox("File not created")
+                userTxt.Clear()
+                passTxt.Clear()
+                Exit Sub
+            End If
+        End If
+
+        userTxt.Clear()
+        passTxt.Clear()
+
+    End Sub
+
+    Private Sub binBtn_Click(sender As Object, e As EventArgs) Handles binBtn.Click
+        Dim binPath As String = "C:\Work\Research\Visual Basic\UserFileDB\Resources\userBin.ccorp"
+
+        Dim writer As BinaryWriter
+
+        Dim createFileAns
+        Dim userString As String
+
+        userString = userTxt.Text & "," & passTxt.Text & vbCrLf
+
+
+        'Binary
+        If File.Exists(binPath) Then
+            If checkIfFileEmpty(binPath) Then
+                MsgBox("File is empty.  Writing")
+                writer = New BinaryWriter(File.Open(binPath, FileMode.Create))
+                writer.Write(userString)
+                writer.Close()
+            Else
+                MsgBox("File is not empty.  Appending.")
+                writer = New BinaryWriter(File.Open(binPath, FileMode.Append))
+                writer.Write(userString)
+                writer.Close()
+            End If
+        Else
+            createFileAns = MsgBox("File doesn't exist.  Create it?", MsgBoxStyle.YesNo)
+            If createFileAns = vbYes Then
+                MsgBox("File created")
+                File.Create(binPath)
+            Else
+                MsgBox("File not created")
+                userTxt.Clear()
+                passTxt.Clear()
+                Exit Sub
+            End If
+        End If
+
+        userTxt.Clear()
+        passTxt.Clear()
+
+    End Sub
+
+    Private Function checkIfFileEmpty(tempPath As String) As Boolean
+        Dim tempContent As String
+        Dim sr As New StreamReader(tempPath)
+
+        tempContent = sr.ReadToEnd
+        sr.Close()
+
+        If tempContent.Equals(String.Empty) Then
+            Return True
+        Else
+            Return False
+        End If
+
+    End Function
+
+    Private Sub loadPlainBtn_Click(sender As Object, e As EventArgs) Handles loadPlainBtn.Click
+
+        Dim userSet As New DataTable
+        Dim userRow As DataRow
+
+        userSet.Columns.Add("Username", GetType(String))
+        userSet.Columns.Add("Password", GetType(String))
+
+        Dim plainPath As String = "C:\Work\Research\Visual Basic\UserFileDB\Resources\userPlain.ccorp"
+        Dim tempContent As String
+        Dim splitString
+
+        If File.Exists(plainPath) Then
+            If checkIfFileEmpty(plainPath) Then
+                MsgBox("File is empty, nothing to read")
+                Exit Sub
+            Else
+                Dim sr As New StreamReader(plainPath)
+                Do
+                    tempContent = sr.ReadLine()
+                    If IsNothing(tempContent) Then
+                        Exit Do
+                    Else
+                        userRow = userSet.NewRow
+                        splitString = Split(tempContent, ",")
+                        userRow("Username") = splitString(0)
+                        userRow("Password") = splitString(1)
+                        userSet.Rows.Add(userRow)
+                    End If
+                Loop
+                sr.Close()
+                DataGridView1.DataSource = userSet
+            End If
+        Else
+            MsgBox("File doesn't exist")
+            Exit Sub
+        End If
+
+    End Sub
+
+    Private Sub loadBinBtn_Click(sender As Object, e As EventArgs) Handles loadBinBtn.Click
+        Dim userSet As New DataTable
+        Dim userRow As DataRow
+
+        userSet.Columns.Add("Username", GetType(String))
+        userSet.Columns.Add("Password", GetType(String))
+
+        Dim binPath As String = "C:\Work\Research\Visual Basic\UserFileDB\Resources\userBin.ccorp"
+        Dim tempContent As String
+        Dim splitString
+
+        If File.Exists(binPath) Then
+            If checkIfFileEmpty(binPath) Then
+                MsgBox("File is empty, nothing to read")
+                Exit Sub
+            Else
+                Dim reader As New BinaryReader(File.Open(binPath, FileMode.Open))
+
+                Do
+                    If reader.PeekChar() <> -1 Then
+                        tempContent = Trim(reader.ReadString)
+                        userRow = userSet.NewRow
+                        splitString = Split(tempContent, ",")
+                        userRow("Username") = splitString(0)
+                        userRow("Password") = splitString(1)
+                        userSet.Rows.Add(userRow)
+                    Else
+                        Exit Do
+                    End If
+                Loop
+                reader.Close()
+                DataGridView1.DataSource = userSet
+            End If
+        Else
+            MsgBox("File doesn't exist")
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub userEncyptedBtn_Click(sender As Object, e As EventArgs) Handles userEncyptedBtn.Click
+        Dim plainPath As String = "C:\Work\Research\Visual Basic\UserFileDB\Resources\userEnc.ccorp"
+
+        Dim createFileAns
+        Dim userString As String
+        Dim keyString As String = "checkEng00"
+
+        userString = userTxt.Text & "," & passTxt.Text
+
+        'Plain Test
+        If File.Exists(plainPath) Then
+            If checkIfFileEmpty(plainPath) Then
+                MsgBox("File is empty.  Writing")
+                Dim wrapper As New Simple3Des(keyString)
+                Dim cipherText As String = wrapper.encryptData(userString)
+                File.WriteAllText(plainPath, cipherText & vbCrLf)
+            Else
+                MsgBox("File is not empty.  Appending.")
+                Dim wrapper As New Simple3Des(keyString)
+                Dim cipherText As String = wrapper.encryptData(userString)
+                File.AppendAllText(plainPath, cipherText & vbCrLf)
+            End If
+        Else
+            createFileAns = MsgBox("File doesn't exist.  Create it?", MsgBoxStyle.YesNo)
+            If createFileAns = vbYes Then
+                MsgBox("File created")
+                File.Create(plainPath)
+            Else
+                MsgBox("File not created")
+                userTxt.Clear()
+                passTxt.Clear()
+                Exit Sub
+            End If
+        End If
+
+        userTxt.Clear()
+        passTxt.Clear()
+
+    End Sub
+
+    Private Sub loadEncryptedBtn_Click(sender As Object, e As EventArgs) Handles loadEncryptedBtn.Click
+        Dim userSet As New DataTable
+        Dim userRow As DataRow
+
+        userSet.Columns.Add("Username", GetType(String))
+        userSet.Columns.Add("Password", GetType(String))
+
+        Dim plainPath As String = "C:\Work\Research\Visual Basic\UserFileDB\Resources\userEnc.ccorp"
+        Dim tempContent As String
+        Dim splitString
+        Dim decText As String
+
+        If File.Exists(plainPath) Then
+            If checkIfFileEmpty(plainPath) Then
+                MsgBox("File is empty, nothing to read")
+                Exit Sub
+            Else
+                Dim wrapper As New Simple3Des("checkEng00")
+                Dim sr As New StreamReader(plainPath)
+                Do
+                    tempContent = sr.ReadLine()
+                    If IsNothing(tempContent) Then
+                        Exit Do
+                    Else
+                        decText = wrapper.decryptData(tempContent)
+                        userRow = userSet.NewRow
+                        splitString = Split(decText, ",")
+                        userRow("Username") = splitString(0)
+                        userRow("Password") = splitString(1)
+                        userSet.Rows.Add(userRow)
+                    End If
+                Loop
+                sr.Close()
+                DataGridView1.DataSource = userSet
+            End If
+        Else
+            MsgBox("File doesn't exist")
+            Exit Sub
+        End If
+        'Read the file
+        'Decrpyt the data
+        'Show the data
+    End Sub
+End Class
